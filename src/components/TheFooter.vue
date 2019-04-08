6f3325e80cfe50c34e873807b4e1852ade47706e
Mike Solomon <mike@mikesolomon.org>
2019-04-08 10:51:55 +0300

    Changes footer

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -2,13 +2,14 @@
   <footer>
     <div class="container">
       <router-link class="logo-font" :to="{ name: 'home', params: {} }">
-        conduit
+        Vue.js // Berlin
       </router-link>
       <span class="attribution">
-        An interactive learning project from
+        Mad props to
         <a rel="noopener noreferrer" target="blank" href="https://thinkster.io"
           >Thinkster</a
-        >. Code &amp; design licensed under MIT.
+        >, whose work we modified to make this possible. Code &amp; design
+        licensed under MIT.
       </span>
     </div>
   </footer>
